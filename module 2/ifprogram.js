bc39b3f694b844393cdd3e22d9602e30979f77a2
shravanisbts <shravani.sbts@gmail.com>
2023-06-28 12:37:03 +0530

    save 3 files

---
+++ module 2/ifprogram.js
@@ -0,0 +1,21 @@
+a=20
+if (a>80
+    {
+        console.log("A grade");
+    }
+    else if (a>60)
+    {
+        console.log("B grade")
+    }
+    else if (a>60)
+    {
+        console.log("B grade")
+    }
+    else if (a>40)
+    {
+        console.log("C grade")
+    }
+    else{
+        
+    }
+    
